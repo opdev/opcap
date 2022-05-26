232f0780877007aa737eb7ca9727454897ee3a5c
Yash Oza <yoyashoza@gmail.com>
2022-05-26 11:43:33 -0400

    methods to create and delete secrets
    Signed-off-by: Yash Oza <yoyashoza@gmail.com>

---
+++ internal/operator/secret.go
@@ -0,0 +1,46 @@
+package operator
+
+import (
+	"context"
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+	corev1 "k8s.io/api/core/v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtimeClient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func (oe operatorClient) CreateSecret(ctx context.Context, name string, content map[string]string, secretType corev1.SecretType, namespace string) (*corev1.Secret, error) {
+	secret := corev1.Secret{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Secret",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		StringData: content,
+		Type:       secretType,
+	}
+	err := oe.Client.Create(ctx, &secret, &runtimeClient.CreateOptions{})
+	if err != nil {
+		log.Error(fmt.Errorf("%w: error while creating secret: %s in namespace: %s", err, name, namespace))
+		return nil, err
+	}
+
+	log.Debugf("Secret %s created successfully in namespace %s", name, namespace)
+	return &secret, nil
+}
+
+func (oe operatorClient) DeleteSecret(ctx context.Context, name string, namespace string) error {
+	secret := corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+	log.Debugf("Deleting secret %s from namespace %s", name, namespace)
+	return oe.Client.Delete(ctx, &secret, &runtimeClient.DeleteOptions{})
+}
