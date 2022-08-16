f7b91c8a10f44ae6106c2f1e9df6def37d2d1850
Edmund Ochieng <ochienged@gmail.com>
2022-08-16 10:08:27 -0500

    Use controller runtime GetConfig() function
    Use the controller-runtime GetConfig() function to get the Kubernetes
    config to be used to access cluster resources. This is preferred since it
    utilizes different strategies to obtain the kubernetes config.
    
    All client initializations have also been moved to
    internal/operator/client.go
    
    Closes #88
    
    Signed-off-by: Edmund Ochieng <ochienged@gmail.com>

--- internal/operator/version.go
+++ internal/operator/version.go
@@ -2,29 +2,9 @@ package operator
 
 import (
 	"context"
-	"os"
 
-	configv1 "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/tools/clientcmd"
 )
-
-// getConfigClient returns a OpenShift Config clientset used to get the ClusterVersion resource
-// in order to determine the version of the OpenShift cluster used during opcap run
-func getConfigClient() *configv1.ConfigV1Client {
-	// create openshift config clientset
-	cfg, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
-	if err != nil {
-		logger.Errorf("Unable to build config from flags: %s", err)
-	}
-
-	clientset, err := configv1.NewForConfig(cfg)
-	if err != nil {
-		logger.Errorf("Unable to create new OpenShift Config clientset: %s", err)
-	}
-
-	return clientset
-}
 
 // GetOpenShiftVersion uses the OpenShift Config clientset to get a ClusterVersion resource which has the
 // version of an OpenShift cluster
@@ -33,7 +13,10 @@ func (c operatorClient) GetOpenShiftVersion() (string, error) {
 	var version string
 
 	// OpenShift Config client used to talk with the OpenShift API
-	configClient := getConfigClient()
+	configClient, err := NewConfigClient()
+	if err != nil {
+		return "", err
+	}
 
 	cversions, err := configClient.ClusterVersions().List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
