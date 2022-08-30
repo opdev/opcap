d9bc1ba011ba3553f6524e8b4d207924b31638a4
acmenezes <adcmenezes@gmail.com>
2022-08-30 15:47:54 -0400

    COMBINE lines for better reading
    Signed-off-by: acmenezes <adcmenezes@gmail.com>

--- internal/capability/operator_install.go
+++ internal/capability/operator_install.go
@@ -13,8 +13,7 @@ func (ca *capAudit) OperatorInstall() error {
 	logger.Debugw("installing package", "package", ca.subscription.Package, "channel", ca.subscription.Channel, "installmode", ca.subscription.InstallModeType)
 
 	// create operator's own namespace
-	_, err := operator.CreateNamespace(context.Background(), ca.namespace)
-	if err != nil {
+	if _, err := operator.CreateNamespace(context.Background(), ca.namespace); err != nil {
 		return err
 	}
 
@@ -29,17 +28,16 @@ func (ca *capAudit) OperatorInstall() error {
 	ca.client.CreateOperatorGroup(context.Background(), ca.operatorGroupData, ca.namespace)
 
 	// create subscription for operator package/channel
-	_, err = ca.client.CreateSubscription(context.Background(), ca.subscription, ca.namespace)
-	if err != nil {
+	if _, err := ca.client.CreateSubscription(context.Background(), ca.subscription, ca.namespace); err != nil {
 		logger.Debugf("Error creating subscriptions: %w", err)
 		return err
 	}
 
 	// Get a Succeeded or Failed CSV with one minute timeout
+	var err error
 	ca.csv, err = ca.client.GetCompletedCsvWithTimeout(ca.namespace, ca.csvWaitTime)
 
 	if err != nil {
-
 		// If error is timeout than don't log phase but timeout
 		if err.Error() == "operator install timeout" {
 			ca.csvTimeout = true
