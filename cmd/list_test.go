772311548c57221553d93ae10f2ee21a6c7dfb44
Brad P. Crochet <brad@redhat.com>
2022-11-02 10:44:46 -0400

    Remove client requirement from list command
    The base list command should not create a k8s client. Only the
    'list packages' command needs the client. The upcoming 'list bundles'
    command does not. So, this patch moves the client into the child
    command instead of the base 'list' command.
    
    Fixes #308
    
    Signed-off-by: Brad P. Crochet <brad@redhat.com>

--- cmd/list_test.go
+++ cmd/list_test.go
@@ -1,30 +1,22 @@
 package cmd
 
 import (
-	"os"
-
 	. "github.com/onsi/ginkgo/v2/dsl/core"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Package CMD", func() {
-	BeforeEach(func() {
-		DeferCleanup(os.Setenv, "KUBECONFIG", os.Getenv("KUBECONFIG"))
-		os.Unsetenv("KUBECONFIG")
-	})
+var _ = Describe("List CMD", func() {
 	When("Initializing the command", func() {
-		It("should fail", func() {
+		It("should not error", func() {
 			cmd := listCmd()
-			// If PreRunE && Run are not nil, there was a failure
-			Expect(cmd.PreRunE).ToNot(BeNil())
-			Expect(cmd.Run).ToNot(BeNil())
+			Expect(cmd.PreRunE).To(BeNil())
+			Expect(cmd.Run).To(BeNil())
 		})
 	})
 	When("Executing the command", func() {
-		It("should fail", func() {
-			out, err := executeCommand(listCmd())
-			Expect(err).To(HaveOccurred())
-			Expect(out).To(ContainSubstring("unable to establish kubeconfig"))
+		It("should not error", func() {
+			_, err := executeCommand(listCmd())
+			Expect(err).ToNot(HaveOccurred())
 		})
 	})
 })
